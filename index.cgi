35327f7675e5b0b1d15a441d7bb1f336f0738344
Jakub Podolak <jp429585@students.mimuw.edu.pl>
2021-12-21 01:54:42 +0100

    Multiple fixes, now routing works on server

--- index.cgi
+++ index.cgi
@@ -4,5 +4,13 @@ from wsgiref.handlers import CGIHandler
 
 from index import app
 
-CGIHandler().run(app)
+class ProxyFix(object):
+   def __init__(self, app):
+      self.app = app
 
+   def __call__(self, environ, start_response):
+      return self.app(environ, start_response)
+
+if __name__ == '__main__':
+  app.wsgi_app = ProxyFix(app.wsgi_app)
+  CGIHandler().run(app)
--- index.py
+++ index.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template, url_for
 import psycopg2
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/erd/')
-def script():
+def erd():
    return render_template('erd.html')
+
+@app.route('/script/')
+def script():
+   return render_template('script.html')
+
+def has_no_empty_params(rule):
+   defaults = rule.defaults if rule.defaults is not None else ()
+   arguments = rule.arguments if rule.arguments is not None else ()
+   return len(defaults) >= len(arguments)
 
 @app.route('/')
 def index():
-   return render_template('start.html')
+   links = []
+   for rule in app.url_map.iter_rules():
+      if "GET" in rule.methods and has_no_empty_params(rule):
+         url = url_for(rule.endpoint, **(rule.defaults or {}))
+         links.append((url, rule.endpoint))
+
+   return render_template('start.html', links=links)
 
 if __name__ == '__main__':
    app.run(debug = True)
+
